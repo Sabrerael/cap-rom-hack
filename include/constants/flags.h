64bdf597a9bf4617bf37ddd5439d3cd751244529
Andrew <sabrerael@hotmail.com>
2025-04-17 09:47:21 -0600

    Initial map updates

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1502,19 +1502,19 @@
 
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
-#define FLAG_UNUSED_0x8E5                           (SYSTEM_FLAGS + 0x85) // Unused Flag
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
-#define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
-#define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
-#define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
-#define FLAG_UNUSED_0x8EA                           (SYSTEM_FLAGS + 0x8A) // Unused Flag
-#define FLAG_UNUSED_0x8EB                           (SYSTEM_FLAGS + 0x8B) // Unused Flag
-#define FLAG_UNUSED_0x8EC                           (SYSTEM_FLAGS + 0x8C) // Unused Flag
-#define FLAG_UNUSED_0x8ED                           (SYSTEM_FLAGS + 0x8D) // Unused Flag
-#define FLAG_UNUSED_0x8EE                           (SYSTEM_FLAGS + 0x8E) // Unused Flag
-#define FLAG_UNUSED_0x8EF                           (SYSTEM_FLAGS + 0x8F) // Unused Flag
-#define FLAG_UNUSED_0x8F0                           (SYSTEM_FLAGS + 0x90) // Unused Flag
-#define FLAG_UNUSED_0x8F1                           (SYSTEM_FLAGS + 0x91) // Unused Flag
+#define FLAG_VISITED_STARTING_TOWN                  (SYSTEM_FLAGS + 0x85)
+#define FLAG_VISITED_MIDDLE_TOWN_NORTH              (SYSTEM_FLAGS + 0x86)
+#define FLAG_VISITED_MIDDLE_TOWN_EAST               (SYSTEM_FLAGS + 0x87)
+#define FLAG_VISITED_MIDDLE_TOWN_SOUTH              (SYSTEM_FLAGS + 0x88)
+#define FLAG_VISITED_MIDDLE_TOWN_WEST               (SYSTEM_FLAGS + 0x89)
+#define FLAG_VISITED_CASTLE_TOWN_ANDY               (SYSTEM_FLAGS + 0x8A)
+#define FLAG_VISITED_CASTLE_TOWN_BLINDJIM           (SYSTEM_FLAGS + 0x8B)
+#define FLAG_VISITED_CASTLE_TOWN_HERMES             (SYSTEM_FLAGS + 0x8C)
+#define FLAG_VISITED_CASTLE_TOWN_SABRE              (SYSTEM_FLAGS + 0x8D)
+#define FLAG_VISITED_CASTLE_TOWN_SALSA              (SYSTEM_FLAGS + 0x8E)
+#define FLAG_VISITED_CASTLE_TOWN_TMONEY             (SYSTEM_FLAGS + 0x8F)
+#define FLAG_VISITED_CASTLE_TOWN_TOT                (SYSTEM_FLAGS + 0x90)
+#define FLAG_VISITED_CASTLE_TOWN_TREBLE             (SYSTEM_FLAGS + 0x91)
 #define FLAG_UNUSED_0x8F2                           (SYSTEM_FLAGS + 0x92) // Unused Flag
 #define FLAG_UNUSED_0x8F3                           (SYSTEM_FLAGS + 0x93) // Unused Flag
 #define FLAG_UNUSED_0x8F4                           (SYSTEM_FLAGS + 0x94) // Unused Flag
